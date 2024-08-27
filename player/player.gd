4e30a9db447ec355aca8812550abc34b336d9d2f
Luan Baleeiro <dev.baleeiro@gmail.com>
2024-08-27 09:42:34 -0300

    [feat] Add teste level and basic player character

---
+++ player/player.gd
@@ -0,0 +1,75 @@
+class_name Player extends CharacterBody3D
+
+@export var CAMERA_CONTROLLER : Camera3D
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var MOUSE_SENSITIVITY : float = 0.5
+
+@export var SPEED : float = 5.0
+@export var ACCELERATION : float = 0.1
+@export var DECELERATION : float = 0.25
+
+# Camera & Movement
+var _rotation_input : float
+var _tilt_input : float
+var _mouse_input : bool = false
+var _mouse_rotation : Vector3
+var _player_rotation : Vector3
+var _camera_rotation : Vector3
+var _current_rotation : float
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	_update_gravity(delta)
+	_update_input(SPEED, ACCELERATION, DECELERATION)
+	_update_velocity()
+	_update_camera(delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input:
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+
+func _update_camera(delta) -> void:
+	
+	_current_rotation = _rotation_input
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	_player_rotation = Vector3(0.0, _mouse_rotation.y, 0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x, 0.0, 0.0)
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+	global_transform.basis = Basis.from_euler(_player_rotation)
+	
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	_rotation_input = 0.0
+	_tilt_input = 0.0
+
+func _update_input(speed : float, acceleration : float, deceleration : float) -> void:
+	
+	var _input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(_input_dir.x, 0.0, _input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = lerp(velocity.x, direction.x * speed, acceleration)
+		velocity.z = lerp(velocity.z, direction.z * speed, acceleration)
+	else:
+		velocity.x = move_toward(velocity.x, 0, deceleration)
+		velocity.z = move_toward(velocity.z, 0, deceleration)
+
+func _update_gravity(delta) -> void:
+	velocity.y -= gravity * delta
+
+func _update_velocity() -> void:
+	move_and_slide()
